4c0522d8659b74357e8e7bc9a236d25cacbd341a
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2024-08-29 20:25:39 +0000

    removed to_vector

--- sources/ttml/core/range_utils.hpp
+++ sources/ttml/core/range_utils.hpp
@@ -1,9 +1,1 @@
 #pragma once
-
-namespace ttnn::core {
-template <std::ranges::range R>
-auto to_vector(R&& r) {
-    auto r_common = r | std::views::common;
-    return std::vector(r_common.begin(), r_common.end());
-}
-}  // namespace ttnn::core
