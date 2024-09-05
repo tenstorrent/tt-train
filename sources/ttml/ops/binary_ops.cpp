28eae14dbb14d3995b311be0debfe1fc1598b930
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2024-09-05 02:13:58 +0000

    added add and mul

--- sources/ttml/ops/binary_ops.cpp
+++ sources/ttml/ops/binary_ops.cpp
@@ -14,14 +14,6 @@ ttml::autograd::TensorPtr operator+(const autograd::TensorPtr& a, const autograd
     autograd::GradFunction grad = [a, b, out]() {
         tt::tt_metal::MemoryConfig mem_config;
 
-        // uint8_t queue_id,
-        // const Tensor& grad,
-        // const Tensor& input,
-        // const Tensor& other,
-        // const MemoryConfig& output_mem_config,
-        // const std::vector<bool>& are_required_outputs,
-        // std::optional<Tensor> input_grad,
-        // std::optional<Tensor> other_grad
         auto res = ttnn::add_bw(out->get_grad(), a->get_value(), b->get_value(), mem_config);
 
         a->add_grad(res[0]);
@@ -42,4 +34,33 @@ ttml::autograd::TensorPtr operator+(const autograd::TensorPtr& a, const autograd
 
     return out;
 }
+
+ttml::autograd::TensorPtr operator*(const autograd::TensorPtr& a, const autograd::TensorPtr& b) {
+    autograd::TensorPtr out;
+
+    out->set_value(ttnn::multiply(a->get_value(), b->get_value()));
+    autograd::GradFunction grad = [a, b, out]() {
+        tt::tt_metal::MemoryConfig mem_config;
+
+        auto res = ttnn::mul_bw(0, out->get_grad(), a->get_value(), b->get_value(), mem_config);
+
+        a->add_grad(res[0].value());
+        b->add_grad(res[1].value());
+    };
+    std::vector<autograd::NodeId> links;
+
+    const auto& a_node = a->get_node();
+    const auto& b_node = b->get_node();
+    if (a_node.has_value()) {
+        links.push_back(a_node.value());
+    }
+
+    if (b_node.has_value()) {
+        links.push_back(b_node.value());
+    }
+    out->set_node(autograd::ctx().add_backward_node(std::move(grad), links));
+
+    return out;
+}
+
 }  // namespace ttml::ops
--- sources/ttml/ops/binary_ops.hpp
+++ sources/ttml/ops/binary_ops.hpp
@@ -1,8 +1,8 @@
 #pragma once
-#include "autograd/auto_context.hpp"
 #include "autograd/tensor.hpp"
-
 namespace ttml::ops {
 
 autograd::TensorPtr operator+(const autograd::TensorPtr& a, const autograd::TensorPtr& b);
-}
+autograd::TensorPtr operator*(const autograd::TensorPtr& a, const autograd::TensorPtr& b);
+
+}  // namespace ttml::ops
