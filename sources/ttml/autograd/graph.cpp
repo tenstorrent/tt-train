5ffa67bcc04c74d836c2d8c3b8eb1bc2016171e0
Roman Furko <rfurko@tenstorrent.com>
2024-09-11 17:33:45 +0000

    clean up tensor and graph

--- sources/ttml/autograd/graph.cpp
+++ sources/ttml/autograd/graph.cpp
@@ -2,18 +2,20 @@
 
 namespace ttml::autograd {
 
-const std::vector<std::vector<size_t>>& Graph::get_edges() { return m_links; }
+const std::vector<std::vector<size_t>>& Graph::get_edges() const { return m_links; }
 
-const std::vector<GraphNode>& Graph::get_graph_nodes() { return m_graph_nodes; }
+const std::vector<GraphNode>& Graph::get_graph_nodes() const { return m_graph_nodes; }
 
 NodeId Graph::add_node(GradFunction&& grad_function, std::span<NodeId> links) {
     size_t curr_id = m_graph_nodes.size();
     m_graph_nodes.emplace_back(std::move(grad_function));
-    std::vector<size_t> node_links;
-    for (auto& link : links) {
+
+    auto& node_links = m_links.emplace_back();
+    node_links.reserve(links.size());
+    for (const auto& link : links) {
         node_links.push_back(link.get_id());
     }
-    m_links.push_back(std::move(node_links));
+
     return {curr_id, this};
 }
 
