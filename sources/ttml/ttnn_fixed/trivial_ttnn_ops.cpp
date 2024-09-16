4533e336061043be08450e2da7618879cb335fba
Roman Furko <rfurko@tenstorrent.com>
2024-09-16 16:25:07 +0000

    update tt-metal main branch

--- sources/ttml/ttnn_fixed/trivial_ttnn_ops.cpp
+++ sources/ttml/ttnn_fixed/trivial_ttnn_ops.cpp
@@ -5,7 +5,7 @@
 namespace ttml::ttnn_fixed {
 
 tt::tt_metal::Tensor sum_over_batch(const tt::tt_metal::Tensor& t) {
-    auto res = ttnn::moreh_sum(t, /* dim */ 0, /* keep_dim */ true);
+    auto res = ttnn::moreh_sum(t, /* dim */ 0, /* keep_dim */ true, std::nullopt, std::nullopt, std::nullopt);
     return res;
 }
 
