657cd4cf76d8b86353ec57772a20b71e288ed66e
Roman Furko <rfurko@tenstorrent.com>
2024-09-18 03:29:00 +0000

    Add comment to create_tensor method

--- sources/ttml/autograd/tensor.hpp
+++ sources/ttml/autograd/tensor.hpp
@@ -46,6 +46,7 @@ private:
 
 using TensorPtr = std::shared_ptr<Tensor>;
 
+// TODO: In future implement create tensor without variadic templates to help with code hints in IDE
 template <typename... Args>
 TensorPtr create_tensor(Args &&... args) {
     return std::make_shared<Tensor>(std::forward<Args>(args)...);
