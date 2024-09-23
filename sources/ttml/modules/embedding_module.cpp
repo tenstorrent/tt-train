31e9229ad27e6f66d7f4c0fc043cdeb57af89630
Roman Furko <rfurko@tenstorrent.com>
2024-09-23 03:17:04 +0000

    Use upgraded initialization flow

--- sources/ttml/modules/embedding_module.cpp
+++ sources/ttml/modules/embedding_module.cpp
@@ -1,15 +1,15 @@
 #include "embedding_module.hpp"
 
+#include "init/tensor_initializers.hpp"
 #include "ops/embedding_op.hpp"
 
 namespace ttml::modules {
 
 void Embedding::initialize_tensors(uint32_t num_embeddings, uint32_t embedding_dim) {
     auto* device = &autograd::ctx().get_device();
-    std::vector<float> weight_data((size_t)num_embeddings * embedding_dim);
-    init::normal_init(weight_data, {0.F, 1.F});
-    m_weight = autograd::create_tensor(
-        core::from_vector(weight_data, core::create_shape({1, 1, num_embeddings, embedding_dim}), device));
+    m_weight = autograd::create_tensor();
+    init::normal_init(
+        m_weight, core::create_shape({1, 1, num_embeddings, embedding_dim}), /* normal params */ {0.F, 1.F});
 }
 
 Embedding::Embedding(uint32_t num_embeddings, uint32_t embedding_dim) {
