e67aa0d17aa7a9dff86631096bb6e3ed65f29a27
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2024-09-30 23:44:35 +0000

    fixed deps cmake

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -58,31 +58,5 @@ CPMAddPackage(
   GIT_TAG v1.1.1
 )
 
-# Include FetchContent module
-include(FetchContent)
 
-
-FetchContent_Declare(
-    msgpack
-    GIT_REPOSITORY https://github.com/msgpack/msgpack-c.git
-    GIT_TAG cpp-6.1.0  # You can specify a version tag or branch name
-  
-)
-FetchContent_GetProperties(msgpack)
-FetchContent_Populate(msgpack)
-# Set options before adding the msgpack directory
-set(MSGPACK_BUILD_EXAMPLES OFF CACHE INTERNAL "")
-set(MSGPACK_BUILD_TESTS OFF CACHE INTERNAL "")
-set(MSGPACK_BUILD_DOCS OFF CACHE INTERNAL "")
-set(MSGPACK_ENABLE_CXX ON CACHE INTERNAL "")
-set(MSGPACK_USE_BOOST OFF CACHE INTERNAL "")
-set(MSGPACK_BUILD_HEADER_ONLY ON CACHE INTERNAL "")
-set(MSGPACK_ENABLE_SHARED OFF CACHE INTERNAL "")
-set(MSGPACK_ENABLE_STATIC OFF CACHE INTERNAL "")
-set(MSGPACK_CXX20 ON CACHE INTERNAL "")
-set(MSGPACK_NO_BOOST ON CACHE BOOL "Disable Boost in msgpack" FORCE)
-# Add the msgpack subdirectory
-#add_subdirectory(${msgpack_SOURCE_DIR} ${msgpack_BINARY_DIR} EXCLUDE_FROM_ALL)
-
-# Make msgpack available
-FetchContent_MakeAvailable(msgpack)
+include(${PROJECT_SOURCE_DIR}/cmake/fetch_msgpack.cmake)
