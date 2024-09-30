afc29bbfea274d8e8df7754d01409ef838d63a26
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2024-09-30 23:42:30 +0000

    removed boost lib

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -8,7 +8,7 @@ set(ENV{CPM_SOURCE_CACHE} "${PROJECT_SOURCE_DIR}/.cpmcache")
 include(${PROJECT_SOURCE_DIR}/cmake/fetch_boost.cmake)
 
 fetch_boost_library(smart_ptr)
-fetch_boost_library(predef)
+
 ############################################################################################################################
 # yaml-cpp
 ############################################################################################################################
