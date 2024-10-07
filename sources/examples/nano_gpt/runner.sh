dec6826d348d266581af4bf26968c723ca1bb5ef
Roman Furko <rfurko@tenstorrent.com>
2024-10-07 05:13:04 +0000

    add board reset to runner script

--- sources/examples/nano_gpt/runner.sh
+++ sources/examples/nano_gpt/runner.sh
@@ -2,14 +2,16 @@
 
 export TT_METAL_LOGGER_LEVEL=FATAL
 SCRIPT="/home/ubuntu/ML-Framework-CPP/build/sources/examples/nano_gpt/nano_gpt"
+RESET_BOARD="tt-smi -r 0"
 INTERVAL=100
-DEFAULT_SEED=5489
+DEFAULT_SEED=5488
 MAX_STEPS=5000
 SLEEP_DURATION=30
 
 echo "Running $SCRIPT..."
-for i in {1..10}; do
+for i in {1..5}; do
     $SCRIPT -i $INTERVAL -p transformer.msgpack -s $((DEFAULT_SEED - i)) -m $MAX_STEPS
+    $RESET_BOARD
     echo "Sleeping for $SLEEP_DURATION seconds and restarting training..."
     sleep $SLEEP_DURATION
 done
