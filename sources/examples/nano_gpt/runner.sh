f131c9180dbf9003ecc36d0bae9fcdeb5b614554
Roman Furko <rfurko@tenstorrent.com>
2024-10-08 03:21:13 +0000

    Enable larger LLM training

--- sources/examples/nano_gpt/runner.sh
+++ sources/examples/nano_gpt/runner.sh
@@ -7,11 +7,12 @@ INTERVAL=100
 DEFAULT_SEED=5489
 MAX_STEPS=5000
 SLEEP_DURATION=30
+BATCH_SIZE=8
 
 $RESET_BOARD
 echo "Running $SCRIPT..."
 for i in {1..5}; do
-    $SCRIPT -i $INTERVAL -p transformer_gpt2.msgpack -s $((DEFAULT_SEED - i)) -m $MAX_STEPS
+    $SCRIPT -i $INTERVAL -p transformer_gpt2.msgpack -s $((DEFAULT_SEED - i)) -m $MAX_STEPS -b $BATCH_SIZE
     $RESET_BOARD
     echo "Sleeping for $SLEEP_DURATION seconds and restarting training..."
     sleep $SLEEP_DURATION
--- sources/ttml/ops/linear_op.cpp
+++ sources/ttml/ops/linear_op.cpp
@@ -23,7 +23,7 @@ autograd::TensorPtr linear_op(
         /* dtype */ std::nullopt,
         /* program_config */ std::nullopt,
         /* activation */ std::nullopt,
-        /* compute_kernel_config */ core::ComputeKernelConfig::precise()));
+        /* compute_kernel_config */ core::ComputeKernelConfig::fast()));
 
     autograd::GradFunction grad = [weight, bias, tensor, out]() {
         auto bias_grad = core::zeros_like(bias->get_value());
@@ -42,7 +42,7 @@ autograd::TensorPtr linear_op(
             /* input_grad_mem_config */ std::nullopt,
             /* weight_grad_mem_config */ std::nullopt,
             /* bias_grad_mem_config */ std::nullopt,
-            /* compute_kernel_config */ core::ComputeKernelConfig::precise());
+            /* compute_kernel_config */ core::ComputeKernelConfig::fast());
 
         tensor->add_grad(res[0].value());
         weight->add_grad(res[1].value());
