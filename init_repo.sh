b52c3792f0cde7376981477d362c9dff36b56bbb
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2024-10-10 19:24:30 +0000

    black magic

--- init_repo.sh
+++ init_repo.sh
@@ -3,6 +3,6 @@ git config --local core.hooksPath .githooks/
 chmod +x .githooks/pre-commit
 sudo apt install clang-tidy=1:10.0-50~exp1
 sudo apt install clang-format=1:10.0-50~exp1
-
+sudo apt  install direnv=2.21.2-1
 chmod +x init_tt_metal.sh
 source ./init_tt_metal.sh
--- init_tt_metal.sh
+++ init_tt_metal.sh
@@ -1,6 +1,13 @@
 # Description: This file is used to initialize the environment variables for the TT-Metal project.
 # Called from the init_repo.sh file.
-export ARCH_NAME=wormhole_b0
-export TT_METAL_HOME=${PWD}/tt-metal
-export PYTHONPATH=${PWD}/tt-metal
-export TT_METAL_ENV=dev
+
+if ! grep -q "eval \"\$(direnv hook bash)\"" ~/.bashrc; then
+    echo "eval \"\$(direnv hook bash)\"" >> ~/.bashrc
+    source ~/.bashrc
+fi
+touch .envrc
+echo export  ARCH_NAME=wormhole_b0 >> .envrc
+echo export  TT_METAL_HOME=${PWD}/tt-metal >> .envrc
+echo export  PYTHONPATH=${PWD}/tt-metal >> .envrc
+echo export  TT_METAL_ENV=dev >> .envrc
+direnv allow .
