ff4c742e922b32725f330003581b952815cd8748
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2024-10-10 00:11:53 +0000

    added submodule tt-metal

--- init_repo.sh
+++ init_repo.sh
@@ -1,4 +1,8 @@
+# Description: Initialize the repository with the necessary configurations
 git config --local core.hooksPath .githooks/
 chmod +x .githooks/pre-commit
-sudo apt install clang-tidy
-sudo apt install clang-format
+sudo apt install clang-tidy=1:10.0-50~exp1
+sudo apt install clang-format=1:10.0-50~exp1
+
+chmod +x init_tt_metal.sh
+source ./init_tt_metal.sh
---
+++ init_tt_metal.sh
@@ -0,0 +1,6 @@
+# Description: This file is used to initialize the environment variables for the TT-Metal project.
+# Called from the init_repo.sh file.
+export ARCH_NAME=wormhole_b0
+export TT_METAL_HOME=${PWD}/tt-metal
+export PYTHONPATH=${PWD}/tt-metal
+export TT_METAL_ENV=dev
