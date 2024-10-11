b6075c781c71fb01b6ba361fee7978fd0a7074db
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2024-10-10 17:08:23 -0700

    Added submodule tt-metal (#44)
    * added submodule tt-metal
    
    * black magic
    
    * removed gitingore
    
    * added print
    
    * readme updated

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 CPP ML Framework prototype
 
 # How To Start
-run init_repo.sh
+run `source ./init_repo.sh`
 
 # Some commands to use now
 
@@ -11,3 +11,12 @@ cmake  -DCMAKE_BUILD_TYPE=Debug -B debug -GNinja
 cmake --build debug --config Debug --clean-first
 
 clang-tidy sources/examples/sample_app/main.cpp -p debug
+
+
+# TT-Metal
+`init_tt_metal.sh` is used to setup metal env variables.
+Do not forget to call:
+```
+git submodule update --init --recursive
+```
+And manually rebuild tt-metal if needed
---
+++ init_tt_metal.sh
@@ -0,0 +1,13 @@
+# Description: This file is used to initialize the environment variables for the TT-Metal project.
+# Called from the init_repo.sh file.
+
+if ! grep -q "eval \"\$(direnv hook bash)\"" ~/.bashrc; then
+    echo "eval \"\$(direnv hook bash)\"" >> ~/.bashrc
+    source ~/.bashrc
+fi
+touch .envrc
+echo export  ARCH_NAME=wormhole_b0 >> .envrc
+echo export  TT_METAL_HOME=${PWD}/tt-metal >> .envrc
+echo export  PYTHONPATH=${PWD}/tt-metal >> .envrc
+echo export  TT_METAL_ENV=dev >> .envrc
+direnv allow .
