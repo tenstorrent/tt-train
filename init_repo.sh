9acf6bc52dcbce2035ff041b41e674caa229713b
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2024-10-15 22:08:27 -0700

    Add automatic clang-tidy run&comment on pull request (#65)
    * Update pull_request.yaml
    
    * Update pull_request.yaml
    
    * Update pull_request.yaml
    
    * Update pull_request.yaml
    
    * Add installation of libc++abi-dev
    
    * Restart bash after installing dependencies to load new path
    
    * Clang tools v17
    
    * Attempt to fix
    
    * See installed binaries
    
    * use py script for clang-tidy-diff
    
    * Use full path to clang-tidy-diff
    
    * Nit file format update
    
    * Add obvious clang-tidy error
    
    * remove clang-tidy obvious error
    
    * Update .github/workflows/pull_request.yaml
    
    Co-authored-by: Bryan Wilder Field Lozano <blozano@tenstorrent.com>
    
    * Update init_repo.sh to sync clang-tidy version
    
    ---------
    
    Co-authored-by: Artem Yerofieiev <artem@tenstorrent.com>
    Co-authored-by: Roman Furko <rfurko@tenstorrent.com>
    Co-authored-by: Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
    Co-authored-by: Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>

--- init_repo.sh
+++ init_repo.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Description: Initialize the repository with the necessary configurations
-sudo apt install clang-tidy=1:10.0-50~exp1
+sudo apt install clang-tidy-17
 sudo apt install pre-commit
 pre-commit install
 sudo apt  install direnv=2.21.2-1
