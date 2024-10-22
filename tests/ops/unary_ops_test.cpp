401bd965c76dc9be99930531bce6d627681d399e
Roman Furko <rfurko@tenstorrent.com>
2024-10-22 21:10:35 +0000

    Add logsoftmax tests

--- tests/ops/unary_ops_test.cpp
+++ tests/ops/unary_ops_test.cpp
@@ -32,3 +32,26 @@ TEST(UnaryOpsTest, GlobalMean) {
         EXPECT_FLOAT_EQ(it, 0.125F);
     }
 }
+
+TEST(UnaryOpsTest, LogSoftmax) {
+    auto* device = &ttml::autograd::ctx().get_device();
+    std::vector<float> test_data = {-0.1F, -0.2F, -0.3F, -0.4F, 0.F, -0.2F, -0.3F, -0.4F};
+    auto tensor = ttml::core::from_vector(test_data, ttml::core::create_shape({2, 1, 1, 4}), device);
+    auto tensor_ptr = ttml::autograd::create_tensor(tensor);
+    auto result = ttml::ops::log_softmax(tensor_ptr, 3);
+    auto result_data = ttml::core::to_vector(result->get_value());
+    std::vector<float> expected_data = {
+        -1.24253553F, -1.34253553F, -1.44253553F, -1.54253553F, -1.17244159F, -1.37244159F, -1.47244159F, -1.57244159F};
+    EXPECT_EQ(result_data.size(), expected_data.size());
+    for (uint32_t idx = 0; idx < result_data.size(); ++idx) {
+        EXPECT_NEAR(result_data[idx], expected_data[idx], 2e-2F);
+    }
+
+    result->backward();
+    auto tensor_grad = ttml::core::to_vector(tensor_ptr->get_grad());
+    std::vector<float> expected_grad = {-0.156F, -0.03906F, 0.05078F, 0.1406F, -0.25F, -0.0156F, 0.07421F, 0.16406F};
+    EXPECT_EQ(tensor_grad.size(), expected_grad.size());
+    for (uint32_t idx = 0; idx < tensor_grad.size(); ++idx) {
+        EXPECT_NEAR(tensor_grad[idx], expected_grad[idx], 2e-2F);
+    }
+}
