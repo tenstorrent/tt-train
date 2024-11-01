c7f86a00c2a5270d1a04b533dfd970081181ae79
Roman Furko <rfurko@tenstorrent.com>
2024-11-01 21:24:03 +0000

    Added tests (one of them disabled due to SEGFAULT)

--- sources/ttml/ops/linear_op.hpp
+++ sources/ttml/ops/linear_op.hpp
@@ -11,4 +11,16 @@ namespace ttml::ops {
 autograd::TensorPtr linear_op(
     const autograd::TensorPtr& tensor, const autograd::TensorPtr& weight, const autograd::TensorPtr& bias);
 
+void ttnn_linear_backward(
+    const autograd::TensorPtr& tensor,
+    const autograd::TensorPtr& weight,
+    const autograd::TensorPtr& bias,
+    const autograd::TensorPtr& out);
+
+void moreh_linear_backward(
+    const autograd::TensorPtr& tensor,
+    const autograd::TensorPtr& weight,
+    const autograd::TensorPtr& bias,
+    const autograd::TensorPtr& out);
+
 }  // namespace ttml::ops
