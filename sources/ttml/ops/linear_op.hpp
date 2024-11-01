58ed5ff566e9d3b3f4dc1d995b0d7669ea47e9bb
Roman Furko <rfurko@tenstorrent.com>
2024-11-01 14:59:44 -0700

    Optimize linear backward  (#97)
    * Optimize linear backward 
    
    * Added tests (one of them disabled due to SEGFAULT)
    
    * Add precise mode for linear backward. Disable by default.

--- sources/ttml/ops/linear_op.hpp
+++ sources/ttml/ops/linear_op.hpp
@@ -11,4 +11,18 @@ namespace ttml::ops {
 autograd::TensorPtr linear_op(
     const autograd::TensorPtr& tensor, const autograd::TensorPtr& weight, const autograd::TensorPtr& bias);
 
+void ttnn_linear_backward(
+    const autograd::TensorPtr& tensor,
+    const autograd::TensorPtr& weight,
+    const autograd::TensorPtr& bias,
+    const autograd::TensorPtr& out,
+    bool precise = false);
+
+void moreh_linear_backward(
+    const autograd::TensorPtr& tensor,
+    const autograd::TensorPtr& weight,
+    const autograd::TensorPtr& bias,
+    const autograd::TensorPtr& out,
+    bool precise = false);
+
 }  // namespace ttml::ops
